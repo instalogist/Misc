4dc7035b5c579fb6b000aa9a7ca9850af79e478b
shubhoo6 <shubham141015@gmail.com>
2020-08-21 17:40:40 +0530

    main application

---
+++ application/src/main/java/main/practice/application/topiccontroller/TopicController.java
@@ -0,0 +1,43 @@
+package main.practice.application.topiccontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping("/topic")
+    public List<Topic> getAllTopics ()
+    {
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topic/{id}")
+    public Topic getTopic(@PathVariable String id)
+    {
+       return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value ="/topic")
+    public void addTopic(@RequestBody Topic topic)
+    {
+    topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value ="/topic/{id}")
+    public void updateTopic(@RequestBody Topic topic,@PathVariable String id)
+    {
+        topicService.updateTopic(id,topic);
+    }
+    @RequestMapping(method = RequestMethod.DELETE,value = "/topic/{id}")
+    public void deleteTopic(@PathVariable String id)
+    {
+       topicService.deleteTopic(id);
+    }
+}
